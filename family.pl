055acee44d91bfed937bf596707e689970c5e86c
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2022-01-26 00:39:58 +0900

    rules

--- family.pl
+++ family.pl
@@ -12,3 +12,19 @@ female(pam).
 female(pat).
 female(liz).
 female(ann).
+
+offspring(Y, X) :-
+  parent(X, Y).
+
+mother(X, Y) :-
+  parent(X, Y),
+  female(X).
+
+grandparent(X, Z) :-
+  parent(X, Y),
+  parent(Y, Z).
+
+sister(X, Y) :-
+  parent(Z, X),
+  parent(Z, Y),
+  female(X).
